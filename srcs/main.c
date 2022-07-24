2ac9e6eb2892e3b5a308c4038dfd7d8ec7e6d254
Shielzz <louisa.hafsi@gmail.com>
2022-07-24 19:42:36 +0200

    y'a encore des pti soucis mais tout va bien on avance

--- srcs/main.c
+++ srcs/main.c
@@ -6,36 +6,72 @@
 /*   By: Louisa <Louisa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 03:54:20 by lhafsi            #+#    #+#             */
-/*   Updated: 2022/07/19 14:43:35 by Louisa           ###   ########.fr       */
+/*   Updated: 2022/07/24 19:33:23 by Louisa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
-
-int	ft_count_lines(char *map)
+//check leaks ici
+int	ft_check_map(int fd)
 {
-	int		fd;
-	int		count;
+	char	*last;
 	char	*gnl;
 
+	gnl = get_next_line(fd);
+	if(ft_check_wall(gnl) == 0)
+		return (0);
+	while (gnl != NULL)
+	{
+		last = gnl;
+		printf("\nlast = %s", last);
+		free (gnl);
+		gnl = get_next_line(fd);
+		if (ft_check_wall(gnl) == 1)
+		{
+			printf("check last = %i", ft_check_wall(last));
+			free (gnl);
+			break ;
+		}
+		printf("gnl = %s\n", gnl);
+		if(ft_check_mid(gnl) == 0)
+			return (0);
+	}
+	if(ft_check_wall(last) == 0)
+		return (0);
+	free(gnl);
+	free(last);
+	return(1);
+}
+
+int	ft_manage_gnl(char *map)
+{	
+	int	fd;
+	int	ok;
+
+	ok = -1;
 	fd = open(map, O_RDONLY);
 	if (fd > 0)
 	{
-		count = 0;
-		gnl = get_next_line(fd);
-		while (gnl != NULL)
-		{ 
-			count++;
-			free (gnl);
-			gnl = get_next_line(fd);
-		}
-		close (fd);
-		free (gnl);
-		return (count);
+		ok = ft_check_map(fd);
+		close(fd);
 	}
-	else
-		write(1, "Error counting lines\n", 21);
-	return (-1);
+	return(ok);
+}
+
+int	main(int ac, char **av)
+{
+	int ok;
+
+	ok = ft_manage_gnl(av[1]);
+	if (ac != 2)
+		return (0);
+	if (ok == -1)
+		printf("pb with ft_manage and fd\n");
+	else if (ok == 1)
+		printf("no pb\n");
+	else if (ok == 0)
+		printf("pb with map\n"); 
+	return (0);
 }
 
 // int	main(int ac, char **av)
@@ -45,17 +81,6 @@ int	ft_count_lines(char *map)
 // 	if (ft_count_lines(av[1]) != -1)
 // 		printf("lines: %i\n", ft_count_lines(av[1]));
 // 	else
-// 		printf("Incorrect\n");
+// 		printf("Incorrect\n"); 
 // 	return (0);
 // }
-
-int	main(int ac, char **av)
-{
-	if (ac != 2)
-		return (0);
-	if (ft_count_lines(av[1]) != -1)
-		printf("lines: %i\n", ft_count_lines(av[1]));
-	else
-		printf("Incorrect\n");
-	return (0);
-}
--- srcs/parsing/check_map_utils.c
+++ srcs/parsing/check_map_utils.c
@@ -6,7 +6,7 @@
 /*   By: Louisa <Louisa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 00:13:47 by lhafsi            #+#    #+#             */
-/*   Updated: 2022/07/19 15:16:36 by Louisa           ###   ########.fr       */
+/*   Updated: 2022/07/24 19:05:29 by Louisa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_check_wall(char *line)
 	int	i;
 
 	i = 0;
-	while (line[i])
+	while (line[i] && line[i] != '\n')
 	{	
 		if (line[i] != '1')
 			return (0);
@@ -29,41 +29,36 @@ int	ft_check_wall(char *line)
 int	ft_check_mid(char *line)
 {
 	int	i;
+	int	j;
+	int	e;
 
+	j = ft_strlen(line);
+	printf("len = %i\n", j);
+	printf("i = 0 // line[0] = %c\n", line[0]);
 	if (line[0] != '1')
 		return (0);
 	i = 1;
-	while (line[i + 1])
+	while (line[i] && i < j - 2)
 	{	
 		if (!(line[i] == 'C' || line[i] == 'P' || line[i] == 'E'
 			|| line[i] == '0' || line[i] == '1'))
 			return (0);
-		printf("i = %i\nline[i] = %c\n", i, line[i]);
+		printf("i = %i // line[i] = %c\n", i, line[i]);
 		i++;
 	}
+	printf("i = %i // line[i] = %c\n", i, line[i]);
 	if (line[i] != '1')
 		return (0);
 	return (1);
 }
 
-// int	ft_check_map(char **map)
+// int	main(int ac, char **av)
 // {
-// 	int	i;
-// 	int	last;
-// 	int	line;
-	
-// 	line = ft_strlen(map[i]);
-// 	last = ft_count_lines(line);
-// 	while (map[i])
-// 	{
-// 		if (ft_strlen(map[i]) != line)
-// 			return (0);
-// 		if (ft_check_wall(map[0]) == 0 || ft_check_wall(map[last]) == 0)
-// 			return (0);
-// 		if (ft_check_mid(map[i]) == 1 && i != 0 && i != last)
-// 			return (0);
-// 		i++;
-// 	}
-// 	return (1);
+// 	if (ac != 2)
+// 		return (0);
+// 	if (ft_check_mid(av[1]) != 1)
+// 		printf("mid not ok\n");
+// 	else
+// 		printf("ok\n"); 
+// 	return (0);
 // }
-
